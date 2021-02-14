d1d703b3ec0b614b7ba9b908d54fc0999ffaf4ad
Lena Blechmann <lenablechmann@gmail.com>
2021-02-14 16:42:34 +0100

    determine winner via sums of rows, columns and diagonals

--- scripts/jestUnitTests/checkGameOver.test.js
+++ scripts/jestUnitTests/checkGameOver.test.js
@@ -93,7 +93,7 @@ function checkGameOver (gameArray) {
       winner: ''
     }
 
-    // make a copy of gameArray where all X are 2s and all Os are 1, empties are 1000, while counting empties
+    // make a copy of gameArray where all X are 1s and all Os are zeros, empties are 5000, while counting empties
     let digitsArray = gameArray.map(inner => inner.slice())
 
     let emptiesCounter = 0;
@@ -102,13 +102,13 @@ function checkGameOver (gameArray) {
         for (let col = 0; col < digitsArray[row].length; col++) {
             if (gameArray[row][col] === '') {
                 emptiesCounter++;
-                digitsArray[row][col] = 1000;
+                digitsArray[row][col] = 5000;
             }
             else if (gameArray[row][col] === 'X') {
-                digitsArray[row][col] = 2;
+                digitsArray[row][col] = 1;
             }
             else if (gameArray[row][col] === 'O') {
-                digitsArray[row][col] = 1;
+                digitsArray[row][col] = 0;
             }
             else {
                 console.log("unexpected symbol in the array")
@@ -116,16 +116,47 @@ function checkGameOver (gameArray) {
         }
     }
     console.dir(digitsArray);
-    console.log(emptiesCounter);
+    console.log("There are " + emptiesCounter + " empty cells.");
     if (!emptiesCounter) {
         // if the gameboard is full, game is over
         console.log("no empties")
         winningSummary.gameOver = true;
-        return winningSummary;
     }
+    // sums rows
+    const rowSum = digitsArray.map(r => r.reduce((a, b) => a + b));
+    console.log(`The sum of rows is ${rowSum}`)
+    // loops through the new array, and finds any winning row
+    for (let i = 0; i < rowSum.length; i++) {
+        if (rowSum[i] === 0) {
+            winningSummary.winner = 'O';
+            // the index of rowSum is the corresponding row in the gameBoard
+            // meaning the ids will be 'i' and all numbers from 0 to array.length
+        }
+        else if (rowSum[i] === digitsArray.length) {
+            winningSummary.winner = 'X';
+            // the index of rowSum is the corresponding row in the gameBoard
+            // meaning the ids will be 'i' and all numbers from 0 to array.length
+
+        }
+    }
+
+    // sums of columns
+    const colSum = digitsArray.reduce((a, b) => a.map((x, i) => x + b[i]));
+    console.log(`The sum of columns is ${colSum}`)
+
+    //sum of diagonals
+    const sumDiagonals = {main: 0, second: 0};
+    for (let i = 0; i < digitsArray.length; i++) {
+        sumDiagonals.main += digitsArray[i][i];
+        sumDiagonals.second += digitsArray[i][digitsArray.length-i-1];
+    }
+    console.log(`The sum of diagonal1 is ${sumDiagonals.main}`)
+    console.log(`The sum of diagonal2 is ${sumDiagonals.second}`)
+
     // loop over the array and sum up every row. If row sum === array.lengthx2 then X won
-    // if row sum === array.length, 0 won
-    // if row > 1000, not a winning row
-    // repeat the same with columns and diagonals
+    // if row sum === 0, 0 won
+    // if row > 5000, not a winning row
+    // if column sum === array.length, X won
+
     return winningSummary;
 }
