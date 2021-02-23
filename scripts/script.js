3f00879f1dda814e45d295535d12c2ba9be90a63
Lena Blechmann <lenablechmann@gmail.com>
2021-02-23 19:44:51 +0100

    make minimax return best move

--- scripts/script.js
+++ scripts/script.js
@@ -258,7 +258,7 @@ document.addEventListener("DOMContentLoaded", function () {
         };
 
         // [TODO] minimax function: takes board as input, returns optimal move for the player or empty if game over
-        function minimax(gameArray, maxDepth, player) {
+        function minimax(gameArray) {
             // create object that will have both best value and the appropriate move for O
             // who is a minimizer
             let best = {
@@ -266,11 +266,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 moveID : ''
             };
             // players are 0 and 1, x is 1, 0 is O
-
             // let's say x is 1, and is maximizer (the higher the value, the better). 
             // O is 0 and a minimizer (the lower the value of the board the better)
-            console.log("Depth is: " + maxDepth + " And player is: " + player);
-
             function maximizer(gameArray){
                 const actionsArray = listAllActions(gameArray);
                 if (maxDepth === 0 || checkGameOver(gameArray).gameOver === true){
@@ -304,6 +301,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         // O is minimizer, resultOfAction also copies the array
                         const actionResult = resultOfAction(gameArray, action, 'O');
                         value = Math.min(value, maximizer(actionResult));
+                        // returning the best move, as it goes up the stack (recursively)
                         if (value < best.value) {
                             best.value = value;
                             best.moveID = actionsArray[index];
@@ -312,6 +310,9 @@ document.addEventListener("DOMContentLoaded", function () {
                     }
                 }
             }
+            // [TODO] minimax will only get called for O, so... we can leverage that. and call minimizer immediately
+            minimizer(gameArray);
+            return best.moveID;
         };
 
         return {
